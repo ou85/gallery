92aa8ba5d5a9f3873823010912b15f3b9ab4cc0a
mironov <mironov@toasoft.co.jp>
2022-10-13 11:19:56 +0900

    rolling

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
-import SimpleGallery from "./simple-gallery";
 import MuiGallery from "./mui-gallery";
-import reportWebVitals from "./reportWebVitals";
+import SimpleGallery from "./simple-gallery";
 
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Routes>
       <Route exact path="/" element={<SimpleGallery />} />
-      <Route exact path="/mui-gallery" element={<MuiGallery />} />
       <Route exact path="/gallery" element={<SimpleGallery />} />
+      <Route exact path="/mui-gallery" element={<MuiGallery />} />
     </Routes>
   </Router>
-  // <SimpleGallery />
-  // <React.StrictMode>
-  //   <SimpleGallery />
-  //   {/* <Gallery /> */}
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
